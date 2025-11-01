f55d7be8278acc068ba23e68d4b9ee1696ff9dca
Aditya dalvi <adityadalvi8910@gmail.com>
2025-11-01 15:36:23 +0530

    working on exercies 4.16

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const BlogRouter = require('./controllers/bloglist')
+const UserRouter = require('./controllers/users')
+const { errorHandler } = require('./utils/middleware')
 const app = express()
 const mongoUrl = config.MONGODB_URI
 
@@ -9,5 +11,7 @@ mongoose.connect(mongoUrl)
 
 app.use(express.json())
 app.use('/api/blogs', BlogRouter)
+app.use('/api/users', UserRouter)
+app.use(errorHandler)
 
 module.exports = app
